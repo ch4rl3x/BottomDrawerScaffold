9f068bdb399e9b5b2abd7b5d1e9af15a30ac862b
Alexander Karkossa <alexander.karkossa@googlemail.com>
2021-10-02 19:55:36 +0200

    fix: correct bottom padding to content

--- bottom-drawer-scaffold/src/main/java/de/charlex/compose/BottomDrawerScaffold.kt
+++ bottom-drawer-scaffold/src/main/java/de/charlex/compose/BottomDrawerScaffold.kt
@@ -4,35 +4,11 @@ import androidx.compose.animation.core.AnimationSpec
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.requiredHeightIn
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.FabPosition
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.SnackbarHost
-import androidx.compose.material.SnackbarHostState
-import androidx.compose.material.Surface
-import androidx.compose.material.SwipeableDefaults
-import androidx.compose.material.SwipeableState
-import androidx.compose.material.contentColorFor
-import androidx.compose.material.swipeable
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Stable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -42,13 +18,10 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.semantics.collapse
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.expand
-import androidx.compose.ui.semantics.onClick
-import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.*
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
@@ -90,7 +63,7 @@ fun BottomDrawerScaffold(
         },
         isFloatingActionButtonDocked = floatingActionButton != null && isFloatingActionButtonDocked,
         floatingActionButtonPosition = floatingActionButtonPosition
-    ) {
+    ) { scaffoldPaddingValues ->
         BoxWithConstraints(
             modifier = Modifier,
             contentAlignment = Alignment.BottomCenter
@@ -148,7 +121,7 @@ fun BottomDrawerScaffold(
                                 topBar?.invoke()
 
                                 Box {
-                                    content(PaddingValues(bottom = drawerPeekHeight))
+                                    content(PaddingValues(bottom = max(drawerPeekHeight, scaffoldPaddingValues.calculateBottomPadding())))
 
                                     Scrim(
                                         open = scaffoldState.bottomDrawerState.isExpanded,
